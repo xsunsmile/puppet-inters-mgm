5858c7ecdfae8a8b1038b58c9dd053e522d791c2
Hao SUN <xsunsmile@gmail.com>
2011-06-01 11:56:36 +0900

    add restart cron

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,11 +1,21 @@
 
 class inters::cron {
 
+	exec { 'stop-cron':
+		command => '/etc/init.d/cron stop',
+	}
+
+	exec { 'start-cron':
+		command => '/etc/init.d/cron start',
+		require => Exec['stop-cron'],
+	}
+
 	cron { 'sync_hosts':
 		ensure => present,
 		command => "/usr/bin/mongo_host sync_to_etchosts",
 		user => root,
 		minute => '*/1',
+		require => Exec['start-cron'],
 	}
 
 }
