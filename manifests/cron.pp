3ca3933003a7a7a91e3022553e5e994cfc5f7af7
Hao SUN <xsunsmile@gmail.com>
2011-06-02 12:59:43 +0900

    add puppet cron

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,6 +1,8 @@
 
 class inters::cron {
 
+	$gem_path = gem_path()
+
 	exec { 'stop-cron':
 		command => '/etc/init.d/cron stop',
 	}
@@ -15,6 +17,15 @@ class inters::cron {
 		command => "/usr/bin/mongo_host sync_to_etchosts",
 		user => root,
 		minute => '*/1',
+		require => Exec['start-cron'],
+	}
+
+	cron { 'cron_puppet':
+		environment => "PATH=\$PATH:${gem_path}",
+		ensure => present,
+		command => "puppetd --test --verbose",
+		user => root,
+		minute => '*/30',
 		require => Exec['start-cron'],
 	}
 
