70c598715c9fdeaf62940cfbf2f5f45791e89509
Hao SUN <xsunsmile@gmail.com>
2011-06-02 14:47:18 +0900

    add env for cron

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -3,28 +3,40 @@ class inters::cron {
 
 	$gem_path = gem_path()
 
-	# exec { 'stop-cron':
-	# 	command => '/etc/init.d/cron stop',
-	# }
+	package { 'postfix': ensure => present }
 
-	# exec { 'start-cron':
-	# 	command => '/etc/init.d/cron start',
-	# 	require => Exec['stop-cron'],
-	# }
+	exec { 'stop-cron':
+		command => '/etc/init.d/cron stop',
+		require => Package['postfix'],
+	}
+
+	exec { 'start-cron':
+		command => '/etc/init.d/cron start',
+		require => Exec['stop-cron'],
+	}
+
+	cron { 'set-env':
+		ensure => present,
+		environment => "PATH=/bin:/usr/bin:/usr/local/bin:${gem_path}\nMAILTO=root",
+		command => "echo",
+		month => '1',
+		require => Exec['start-cron'],
+	}
 
 	cron { 'sync_hosts':
 		ensure => present,
 		command => "/usr/bin/mongo_host sync_to_etchosts",
 		user => root,
 		minute => '*/1',
+		require => Cron['set-env'],
 	}
 
 	cron { 'cron_puppet':
-		environment => "PATH=\$PATH:${gem_path}",
 		ensure => present,
 		command => "puppetd --test --verbose | tee -a /tmp/cron_puppet.log",
 		user => root,
 		minute => '*/30',
+		require => Cron['set-env'],
 	}
 
 	cron { 'update_puppet_modules':
@@ -33,6 +45,7 @@ class inters::cron {
 		command => "cd /etc/puppet/modules && sh update.sh | tee -a /tmp/update_puppet.log",
 		user => root,
 		minute => '*/5',
+		require => Cron['set-env'],
 	}
 
 }
